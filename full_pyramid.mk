52f7d7c1497067c9d094f1f202e146359268f5c1
Kryptoxx <kryptoxx@vdbussche.net>
2013-10-23 16:15:42 +0000

    update

--- full_pyramid.mk
+++ full_pyramid.mk
@@ -1,5 +1,5 @@
 # Specify phone tech before including full_phone
-# $(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/r2x2/config/gsm.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := pyramid
@@ -9,7 +9,7 @@ TARGET_SCREEN_HEIGHT := 960
 TARGET_SCREEN_WIDTH := 540
 
 # Inherit some common CM stuff.
-# $(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/r2x2/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/htc/pyramid/device.mk)
@@ -22,4 +22,4 @@ PRODUCT_MODEL := HTC Sensation
 PRODUCT_MANUFACTURER := HTC
 
 # Set build fingerprint / ID / Product Name ect.
-# PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_pyramid BUILD_FINGERPRINT=tmous/htc_pyramid/pyramid:4.0.3/IML74K/356011.14:user/release-keys PRIVATE_BUILD_DESC="3.32.531.14 CL356011 release-keys" BUILD_NUMBER=356011
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_pyramid BUILD_FINGERPRINT=tmous/htc_pyramid/pyramid:4.0.3/IML74K/356011.14:user/release-keys PRIVATE_BUILD_DESC="3.32.531.14 CL356011 release-keys" BUILD_NUMBER=356011
